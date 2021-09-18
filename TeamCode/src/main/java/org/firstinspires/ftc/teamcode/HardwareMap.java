af60b8318fc65c9f6a3bcfac38a394bf44f97c0c
akshay-walia <87461023+akshay-walia@users.noreply.github.com>
2021-09-18 15:11:08 -0700

    Create HardwareMap.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMap.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class HardwareMap {
+    // Create Motors
+    // click enter after typing DcMotor to import the correct directory
+    public DcMotor frontRightMotor = null; // null means not initiated
+    public DcMotor frontLeftMotor = null;
+    public DcMotor backRightMotor = null;
+    public DcMotor backLeftMotor = null;
+
+    // Create Servo
+    // click enter after typing Servo to import the correct directory
+    public Servo randomServo = null;
+
+
+
+}
