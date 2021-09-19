f532ebb2a6d27969ced5f91bdffb7d1dfb218959
akshay-walia <87461023+akshay-walia@users.noreply.github.com>
2021-09-19 15:49:35 -0700

    Update Hardware.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware.java
@@ -2,8 +2,10 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class HardwareMap {
+public class Hardware {
     // Create Motors
     // click enter after typing DcMotor to import the correct directory
     public DcMotor frontRightMotor = null; // null means not initiated
@@ -15,7 +17,18 @@ public class HardwareMap {
     // click enter after typing Servo to import the correct directory
     public Servo randomServo = null;
 
-    
+    // additional variables (don't use public in these, could cause problems)
+           HardwareMap hardwareMap = null;
+    public ElapsedTime runtime = new ElapsedTime();
+
+    // constructor
+    public Hardware(HardwareMap hwMap) {
+        initialize(hwMap);
+    }
+
+    private void initialize(HardwareMap hwMap) {
+
+    }
 
 
 }
