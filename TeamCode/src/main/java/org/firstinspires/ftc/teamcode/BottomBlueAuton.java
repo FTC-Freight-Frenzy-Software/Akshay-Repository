822b78c96aab8fd535de0d46fd0fa0422dac6882
akshay-walia <87461023+akshay-walia@users.noreply.github.com>
2021-10-15 19:54:19 -0700

    Create BottomBlueAuton.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BottomBlueAuton.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "BottomBlueAuton", group = "LinearOpMode")
+
+public class BottomBlueAuton extends LinearOpMode {
+    AutonomousMethods methods = new AutonomousMethods();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // foam tiles are 2 feet (24 inches) long
+        methods.forward(7);
+        methods.turnRight(90);
+        methods.forward(22);
+
+        for (int i = 0; i < 8; i++) {
+            methods.carouselServo.setPower(1);
+            sleep(2000);
+            methods.carouselServo.setPower(0);
+            sleep(1000);
+        }
+    }
+}
