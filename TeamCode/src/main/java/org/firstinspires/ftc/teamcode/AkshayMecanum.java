de05ed91c2ae962c10cef6e84a449240771c22f5
akshay-walia <87461023+akshay-walia@users.noreply.github.com>
2021-10-17 16:51:57 -0700

    Update AkshayMecanum.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AkshayMecanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AkshayMecanum.java
@@ -3,6 +3,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 @TeleOp(name = "AkshayMecanum", group = "LinearOpMode")
 
@@ -13,54 +14,55 @@ public class AkshayMecanum extends LinearOpMode {
     public DcMotor backRight = null;
     public DcMotor frontRight = null;
     public DcMotor intakeMotor = null;
-    public Servo boxServo = null;
     public DcMotor liftMotor = null;
-    public Servo carouselServo = null;
+    public CRServo carouselServo = null;
 
 
     public void runOpMode() {
-        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
-        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
-        backRight = hardwareMap.get(DcMotor.class, "backRight");
-        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        backRight = hardwareMap.get(DcMotor.class, "backRightMotor");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRightMotor");
         intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
-        boxServo = hardwareMap.get(Servo.class, "boxServo");
         liftMotor = hardwareMap.get(DcMotor.class, "liftMotor");
-        carouselServo = hardwareMap.get(Servo.class, "carouselServo");
+        carouselServo = hardwareMap.get(CRServo.class, "carouselServo");
 
         waitForStart();
 
         while(opModeIsActive()) {
-            double horizontal = gamepad1.left_stick_x;
+            double horizontal = -1 * (gamepad1.left_stick_x);
             double vertical = gamepad1.left_stick_y;
-            double turn = gamepad1.right_stick_x;
+            double turn = -1 * (gamepad1.right_stick_x);
 
-            backLeft.setPower(vertical + turn - horizontal);
-            frontLeft.setPower(vertical + turn + horizontal);
+            backLeft.setPower(-1 * (vertical + turn - horizontal));
+            frontLeft.setPower(-1 * (vertical + turn + horizontal));
             backRight.setPower(vertical - turn + horizontal);
             frontRight.setPower(vertical - turn - horizontal);
 
+
             if (gamepad1.right_trigger > 0) {
+                intakeMotor.setPower(-1);
+            } else if (gamepad1.right_bumper) {
                 intakeMotor.setPower(1);
+            } else {
+                intakeMotor.setPower(0);
             }
 
             if (gamepad1.left_bumper) {
-                liftMotor.setPower(1);
+                liftMotor.setPower(.5);
+            } else if (gamepad1.left_trigger > 0) {
+                liftMotor.setPower(-.5);
+            } else {
+                liftMotor.setPower(0);
             }
 
-            if (gamepad1.left_trigger > 0) {
-                boxServo.setPosition(0.8);
+            if (gamepad1.a) {
+                carouselServo.setPower(1);
+            } else if (gamepad1.b) {
+                carouselServo.setPower(-1);
+            } else {
+                carouselServo.setPower(0);
             }
-
-            if (gamepad1.right_bumper) {
-                carouselServo.setPosition(0.7);
-            }
-
-            intakeMotor.setPower(0);
-            liftMotor.setPower(0);
-            boxServo.setPosition(0);
-            carouselServo.setPosition(0);
-
         }
     }
 }
