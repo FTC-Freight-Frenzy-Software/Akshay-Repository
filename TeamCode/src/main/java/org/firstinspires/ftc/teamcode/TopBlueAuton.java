a4ffc0e7bd9fdfa29afd45cb289d6763d88602e9
akshay-walia <87461023+akshay-walia@users.noreply.github.com>
2021-10-17 14:17:33 -0700

    Update TopBlueAuton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TopBlueAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TopBlueAuton.java
@@ -13,14 +13,25 @@ public class TopBlueAuton extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         for (int i = 0; i < 3; i++) {
+            // go forward, then turn left
             methods.forward(3);
             methods.turnLeft90();
             methods.forward(36);
-            // intake freight
+            // intake freight from corner area
+            methods.intakeMotor.setPower(1);
+            sleep(4000);
+            methods.intakeMotor.setPower(0);
+            // go to alliance shipping hub
             methods.reverse(48);
             methods.turnRight90();
             methods.forward(27);
             // put freight in the alliance shipping hub
+            methods.liftServo.setPosition(1);
+            sleep(4000);
+            methods.liftServo.setPosition(0);
+            methods.boxServo.setPosition(1);
+            sleep(2000);
+            methods.boxServo.setPosition(0);
         }
     }
 }
