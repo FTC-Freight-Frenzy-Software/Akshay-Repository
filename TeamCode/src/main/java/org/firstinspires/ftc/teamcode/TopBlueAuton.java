da9b3dd33c39c724825c067cb417d0493e1c4d06
akshay-walia <87461023+akshay-walia@users.noreply.github.com>
2021-10-31 14:22:02 -0700

    Update TopBlueAuton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TopBlueAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TopBlueAuton.java
@@ -8,6 +8,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 @Autonomous(name = "TopBlueAuton", group = "LinearOpMode")
 
 public class TopBlueAuton extends LinearOpMode {
+    AkshayConfiguration methods1 = new AkshayConfiguration();
     AutonomousMethods methods = new AutonomousMethods();
 
     @Override
@@ -18,20 +19,20 @@ public class TopBlueAuton extends LinearOpMode {
             methods.turnLeft90();
             methods.forward(36);
             // intake freight from corner area
-            methods.intakeMotor.setPower(1);
+            methods1.intakeMotor.setPower(1);
             sleep(4000);
-            methods.intakeMotor.setPower(0);
+            methods1.intakeMotor.setPower(0);
             // go to alliance shipping hub
             methods.reverse(48);
             methods.turnRight90();
             methods.forward(27);
             // put freight in the alliance shipping hub
-            methods.liftServo.setPosition(1);
+            methods1.liftMotor.setPower(1);
             sleep(4000);
-            methods.liftServo.setPosition(0);
-            methods.boxServo.setPosition(1);
+            methods1.liftMotor.setPower(0);
+            methods1.boxServo.setPosition(1);
             sleep(2000);
-            methods.boxServo.setPosition(0);
+            methods1.boxServo.setPosition(0);
         }
     }
 }
